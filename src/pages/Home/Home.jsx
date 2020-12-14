2fe8277c7bd9617dd08cc48be92203f8edfe7e4c
Nikolas Lenning <nikolaslenning@gmail.com>
2020-12-13 17:17:30 -0700

    image responsive, moved global css to page specific css file

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import profilePic from "../assets/profileimg.JPG"
-import "../App.css";
+import profilePic from "../../assets/profileimg.JPG"
+import "./Home.css";
 
 const Home = (props) => {
     return (
@@ -8,7 +8,7 @@ const Home = (props) => {
             <div className="row pb-5">
                 <h1 className="page-title col-lg-12 font-weight-bold">About</h1>
                 <div className="col-md-12">
-                    <div className="div-class float-left p-1 pt-2 pr-2"><img className="homeIMG img-fluid rounded" src={profilePic}
+                    <div className="profileIMG float-left p-1 pt- pr-2"><img className="homeIMG img-fluid rounded" src={profilePic}
                         alt="Nikolas"></img></div>
                     <p className="pull-left pl-0 p-1 col-12 col-md-9">Full-stack web developer with a desire to apply my knowledge in Cognitive and Social Psychology to the burgeoning field of software development. Currently earning a certificate in full-stack development from the University of Utah, emphasizing skills in browser-based technologies, such as JavaScript and React.js, server-side development, object-oriented programming, and responsive web design. Self-reliant and effective at thinking abstractly and analytically while problem-solving. Detail-oriented, regardless of the complexity of the problem at hand. I spearheaded a team of four in developing a full-stack application aimed at assisting Autistic individuals in learning and interpreting facial cues and expressions. I am thrilled to utilize my coding abilities in collaborating to build purposeful web applications, while concurrently furthering my knowledge along the way.</p>
                     <p className="pull-left p-1 col-12 col-md-9">My Higher Education was acquired at the University of Utah,
@@ -16,7 +16,7 @@ const Home = (props) => {
                     support myself through the illustrious job of antisocial bartender. Don't worry, the antisocial just
                     ads a layer of mystery.
                 </p>
-                    <div className="pull-left pl-0 p-1 col-9 col-md-6">
+                    <div className=" pl-0 p-1">
                         <div className="text-center rounded-0">
                             <ul className="social-icons">
                                 <p className="social-links">

34d6e22ece52f1d99328eef40128c91a11ef4faf
Nikolas Lenning <nikolaslenning@gmail.com>
2020-12-15 15:49:35 -0700

    added basename variable

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import Header from "./components/Header/Header";
-import PageTitle from "./components/PageTitle/PageTitle"
+// import PageTitle from "./components/PageTitle/PageTitle"
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home"
 import Skills from "./pages/Skills/Skills"

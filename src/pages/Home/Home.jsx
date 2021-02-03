84b15e1dc1059194a403a5709205e3eb4aaf70db
Nikolas Lenning <nikolaslenning@gmail.com>
2021-02-02 18:10:08 -0700

    updated lrg screen colmn size responsiveness

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,7 +9,7 @@ const Home = (props) => {
             <div className="row pl-3 pb-5 mr-1">
                 {/* <h1 className="page-title col-lg-12 font-weight-bold">About</h1> */}
                 <PageTitle />
-                <div className="about mb-4 col-lg-7 col-md-11 col-sm-9">
+                <div className="about mb-4 col-lg-9 col-md-11 col-sm-10">
                     <div className="profileIMG float-left p-1 pt-2 pr-2"><img className="homeIMG img-fluid rounded" src={profilePic}
                         alt="Nikolas"></img></div>
                     <p className="pull-left pl-0 p-1 ">Full-stack web developer with a desire to apply my knowledge in Cognitive and Social Psychology to the burgeoning field of software development. Currently earning a certificate in full-stack development from the University of Utah, emphasizing skills in browser-based technologies, such as JavaScript and React.js, server-side development, object-oriented programming, and responsive web design. Self-reliant and effective at thinking abstractly and analytically while problem-solving. Detail-oriented, regardless of the complexity of the problem at hand. I spearheaded a team of four in developing a full-stack application aimed at assisting Autistic individuals in learning and interpreting facial cues and expressions. I am thrilled to utilize my coding abilities in collaborating to build purposeful web applications, while concurrently furthering my knowledge along the way.</p>

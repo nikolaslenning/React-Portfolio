d5adb2204a6b1fbf658e6b8c370bd7e6b27f3810
Nikolas Lenning <nikolaslenning@gmail.com>
2021-02-08 14:46:04 -0700

    fixed cursor to default for non flipable cards

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,14 +5,14 @@ function Footer(props) {
     // console.log(props)
     return (
         <footer className="container-fluid">
-        <div className="row">
-            <div className="footer col-12 fixed-bottom text-center w-100">
-                <div className="card-class card text-center rounded-0">
-                    <h6 className="card-footer text-muted"> &copy;Copyright 2020</h6>
+            <div className="row">
+                <div className="footer col-12 fixed-bottom text-center w-100">
+                    <div className="card-class card text-center rounded-0">
+                        <h6 className="card-footer text-muted"> &copy;Copyright 2020</h6>
+                    </div>
                 </div>
             </div>
-        </div>
-    </footer>
+        </footer>
     )
 }
 
